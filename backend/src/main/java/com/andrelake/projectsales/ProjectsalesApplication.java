dee59f483e4841fe4a1f78bc8e113308de40d0d2
Andre Lago <andre.lake@hotmail.com>
2021-05-04 19:11:23 -0300

    project created

---
+++ backend/src/main/java/com/andrelake/projectsales/ProjectsalesApplication.java
@@ -0,0 +1,13 @@
+package com.andrelake.projectsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andrelake/projectsales/ProjectsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelake.projectsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
