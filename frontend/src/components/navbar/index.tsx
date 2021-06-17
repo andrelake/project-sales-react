a7516b89b490e54b90d80f9d5e0fd56f027cab20
Andre Lago <andre.lake@hotmail.com>
2021-06-17 18:45:42 -0300

    Home and navigation

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="navbar navbar-dark bg-dark navbar-expand-sm sticky-top">
             <div className={"container-fluid"}>
                 <div className={"row-cols-auto mx-auto float-lg-start"}>
-                    <FontAwesomeIcon className={"icon-color-secondary"} icon={["fas","dollar-sign"]} />
-                    <span className="navbar-brand h1 ms-2">Project Sales</span>
+                    <Link to={"/"} className={"text-decoration-none"}>
+                        <FontAwesomeIcon className={"icon-color-secondary"} icon={["fas","dollar-sign"]} />
+                        <span className="navbar-brand h1 ms-2">Project Sales</span>
+                    </Link>
                 </div>
             </div>
         </nav>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import { Link } from "react-router-dom"
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <Navbar/>
+            <div className={"container"}>
+                <div className={"jumbotron"}>
+                    <h1 className={"display-4"}>Project Sales</h1>
+                    <p className={"lead"}>Now you can see the statistics of your sales</p>
+                    <hr/>
+                    <p>This project shows a sales dashboard that has a backend build with Java and Spring</p>
+                    <Link className={"btn btn-primary btn-lg"} to={"/dashboard"}>
+                        Show Dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer/>
+        </>
+
     )
 }
 
