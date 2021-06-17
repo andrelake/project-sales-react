420048d1d757d2e9775a06f813ab5561fc549f92
Andre Lago <andre.lake@hotmail.com>
2021-06-17 18:20:11 -0300

    frontend - routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,30 @@
+import Navbar from "../../components/navbar";
+import DataTable from "../../components/datatable";
+import BarChart from "../../components/barchart";
+import DonutChart from "../../components/donutchart";
+import Footer from "../../components/footer";
+
+const Dashboard = () => {
+    return (
+        <>
+            <Navbar/>
+            <div className={"container"}>
+                <h1 className={"text-primary text-center py-5"}>DASHBOARD</h1>
+                <DataTable></DataTable>
+                <div className={"row"}>
+                    <div className={"col-sm-6"}>
+                        <h5 className={"text-center"}>Percent of Success</h5>
+                        <BarChart></BarChart>
+                    </div>
+                    <div className={"col-sm-6"}>
+                        <h5 className={"text-center"}>Sales Percentage</h5>
+                        <DonutChart></DonutChart>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default Dashboard;
